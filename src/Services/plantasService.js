4073ee5d3d48a5066f7d3695c977ca3fa0766270
ProjetoPlanta <projetoplanta2025@gmail.com>
2025-03-12 20:34:14 -0300

    qrcode criado

--- src/Services/plantasService.js
+++ src/Services/plantasService.js
@@ -1,6 +1,7 @@
 import api from './ServiceConfig';
 
 const API_URL = '/plantas';
+const QR_CODE_URL = 'api/qrcode';
 
 const PlantaService = {
   cadastrarPlanta: async (planta) => {
@@ -8,25 +9,25 @@ const PlantaService = {
       const response = await api.post(API_URL, planta);
       return response.status;
     } catch (error) {
-      return error
+      return error;
     }
   },
 
-  updatePlanta: async (id,planta) => {
+  updatePlanta: async (id, planta) => {
     try {
       const response = await api.put(`${API_URL}/${id}`, planta);
       return response.status;
     } catch (error) {
-      return {error: error, message:"Erro ao atualizar planta"}
+      return { error: error, message: "Erro ao atualizar planta" };
     }
   },
-  
+
   deletePlanta: async (id) => {
     try {
       const response = await api.delete(`${API_URL}/${id}`);
       return response.status;
     } catch (error) {
-      return {error: error, message:"Erro ao deletar Planta"}
+      return { error: error, message: "Erro ao deletar Planta" };
     }
   },
 
@@ -35,7 +36,7 @@ const PlantaService = {
       const response = await api.get(API_URL);
       return response.data;
     } catch (error) {
-      return {error: error, message:"Erro ao buscar Planta"}
+      return { error: error, message: "Erro ao buscar Planta" };
     }
   },
 
@@ -44,7 +45,16 @@ const PlantaService = {
       const response = await api.get(`${API_URL}/${id}`);
       return response.data;
     } catch (error) {
-      return {error: error, message:"Erro ao buscar Planta"}
+      return { error: error, message: "Erro ao buscar Planta" };
+    }
+  },
+
+  getQRCode: async (id) => {
+    try {
+      const response = await api.get(`${QR_CODE_URL}/${id}`, { responseType: 'blob' });
+      return URL.createObjectURL(response.data); // Cria um objeto URL para exibir a imagem
+    } catch (error) {
+      return { error: error, message: "Erro ao gerar QR Code" };
     }
   }
 };
