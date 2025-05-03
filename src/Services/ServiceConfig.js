6f0f4bb38a9146c3b605c91c7298fade340b9d85
projetoplanta2025 <projetoplanta2025@gmail.com>
2025-05-03 10:43:45 -0300

    Errei a porcaria do httpS

--- src/Services/ServiceConfig.js
+++ src/Services/ServiceConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://projetoplanta-gedsc0epcqetaeht.brazilsouth-01.azurewebsites.net/',
+  baseURL: 'https://projetoplanta-gedsc0epcqetaeht.brazilsouth-01.azurewebsites.net/',
 
   withCredentials:false,
   headers: {
